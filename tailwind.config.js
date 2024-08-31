59240d5c388b5731709df1f4ea3826c6b4aa8b32
JuniorDev1k <ayoubdev1k@gmail.com>
2024-08-31 23:44:50 +0100

    added global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ export default {
 
   theme: {
     extend: {},
+    screens: {
+      'tablet' : '810px' ,
+      'desktop' :  '1200px'
+    },
+    colors : {
+      'primary' : '#FFFFFF',
+      'secondary' : '#0048A6',
+      'highlight' : '#B01799',
+      'hover1' : '#8C0778',
+      'hover2' :" #002B63"
+    }
   },
   plugins: [],
 }

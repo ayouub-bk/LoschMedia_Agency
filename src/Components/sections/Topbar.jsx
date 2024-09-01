7241d39e8add90f8c21139334abe28ffb79d5cbd
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-01 16:07:39 +0100

    feat added topbar

--- src/Components/sections/Topbar.jsx
+++ src/Components/sections/Topbar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Ui/Button";
 import { Link } from "react-router-dom";
+import logo from "../../assets/imgs/Logo.jpg";
 const Topbar = () => {
+  const [toggleSideBar, setToggleSideBar] = useState(false);
   return (
     <>
-      <nav className="h-[63px] flex justify-center items-center">
-        <div>Logo</div>
-        <ul className="flex  ">
+      <nav className=" flex justify-between items-center p-4 px-20">
+        <img className="w-[154px] h-[54px]  " src={logo} alt="" />
+        <ul className="gap-12 text-secondary  text-xl hidden desktop:flex ">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -20,7 +22,36 @@ const Topbar = () => {
             <Link to="/Contact">Contact</Link>{" "}
           </li>
         </ul>
-        <Button />
+        <div className="hidden tablet:block">
+          <Button />
+        </div>
+        <div className="tablet:hidden">
+          <button onClick={() => setToggleSideBar(true)}>==+</button>
+        </div>
+        {toggleSideBar && (
+          <div className=" p-10 bg-green-100 fixed top-0 right-0 bottom-0 left-0 tablet:hidden flex  items-center justify-center  ">
+            <button
+              className="fixed top-0 right-2"
+              onClick={() => setToggleSideBar(false)}
+            >
+              X
+            </button>
+            <ul className="flex flex-col gap-10  ">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Offers">Offers</Link>
+              </li>
+              <li>
+                <Link to="/About">About</Link>
+              </li>
+              <li>
+                <Link to="/Contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </nav>
     </>
   );

65afc793e7220b3c574bf22071891e28875d013a
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-01 16:07:03 +0100

    asssets + buttonUI component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { HomeScreen, About, Testimonials, Pricing, Contact } from "./pages";
 import "./index.css";
+import Topbar from "./Components/sections/Topbar";
 
 const App = () => {
   return (
     <>
       <Routes>
+        {/* <Topbar /> */}
         <Route path="/" element={<HomeScreen />} />
         <Route path="/About" element={<About />} />
         <Route path="/Testimonials" element={<Testimonials />} />

790a17c8de2b5c77b5a776cddaaf0e3d2e2571f0
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-03 16:29:00 +0100

    Our-services UI

---
+++ src/Components/sections/ServicesHome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ServicesHome = () => {
+  return (
+    <div>
+      <h1 className=".highlighted-title">Our Services</h1>
+      {/* services grid cards L  */}
+      <div></div>
+    </div>
+  );
+};
+
+export default ServicesHome;
---
+++ src/data.js
@@ -0,0 +1,19 @@
+import icon1 from "../src/assets/icons/email-marketing.png";
+
+export const services = [
+  {
+    title: "Email marketing",
+    icon: icon1,
+    dettails: [
+      {
+        name: "Data-Driven",
+        description: "Boost your email startegy with expert insights.",
+      },
+      { name: "Triple Your List", description: "Grow your email list by 3x." },
+      {
+        name: "12-Week Plane",
+        description: "Excute a complet newsletter strategy.",
+      },
+    ],
+  },
+];

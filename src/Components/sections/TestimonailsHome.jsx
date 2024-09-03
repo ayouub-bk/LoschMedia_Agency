9da0473fb6a777a3046489ce1657bd6fbda1df53
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-03 16:14:13 +0100

    UI complet testimonails styles

--- src/Components/sections/TestimonailsHome.jsx
+++ src/Components/sections/TestimonailsHome.jsx
@@ -3,7 +3,6 @@ import video from "../../assets/vid/1st-testimonial.mp4";
 import { Link } from "react-router-dom";
 import commmas from "../../assets/icons/commas1.png";
 import profile from "../../assets/imgs/StockImg.jpg";
-import more from "../../assets/icons/more1.png";
 
 const TestimonailsHome = () => {
   return (
@@ -11,42 +10,42 @@ const TestimonailsHome = () => {
       <h1 className="highlighted-title text-secondary tablet:text-[48px] text-[39px]  ">
         Testimonials
       </h1>
-      <div className="flex justify-center desktop:gap-12 w-full pt-6">
-        <div className="desktop:w-[35%] w-full border-secondary border-solid border-8 rounded-md ">
-          <video src={video} width="100%" height="100%" controls></video>
+      <div className="flex flex-col desktop:flex-row   justify-center gap-12 w-full mt-6">
+        <div className="desktop:w-[35%]   w-full  border-secondary border-solid  border-8 rounded-md ">
+          <video className="h-full" src={video} width="100%" controls></video>
         </div>
-        <div className="p-4 shadow-md relative w-[40%] flex flex-col justify-between ">
+        <div className="py-[10px] px-[22px]  shadow-md relative w-full desktop:w-[55%] flex flex-col justify-between  gap-2">
           <img
             className="absolut top-2 left-2"
             src={commmas}
-            height="56px"
+            height="43px"
             width="43"
             alt=""
           />
-          <p>
+          <p className="text-[20px]">
             I want to thank LoshMedia, especially Shahid, for helping me realize
             my agency's mistake. He helped me create a new offer based on
             value, not price. LoshMedia provided a consultation day and didn't
             require upfront payment. They asked for a testimonial only and they
             deserve it. I highly recommend them for scaling your business.
           </p>
-          <div className=" flex justify-between items-center ">
-            <div className="flex gap-2 ">
+          <div className=" flex justify-between items-center  ">
+            <div className="flex gap-4 ">
               <img
                 src={profile}
                 height="60px"
                 width="60px"
-                className="rounded-xl"
+                className="rounded-[50%]"
               />
-              <div>
-                <h2>Kari chahid</h2>
-                <p>LoshMedia CEO</p>
+              <div className="text-secondary">
+                <h2 className="text-[20px]">Kari Chahid</h2>
+                <p className=" opacity-50 text-[14px] ">LoshMedia CEO</p>
               </div>
             </div>
 
             <div className="self-end">
-              <Link to="/testimonial">
-                See all <img src={more} alt="" />{" "}
+              <Link className="text-highlight" to="/testimonial">
+                See all ...
               </Link>
             </div>
           </div>

cc806e528c2cf6483488bd1d16af51c31ae9fcd6
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-03 20:37:57 +0100

    feat hoverEffect content

--- src/Components/sections/OffersHome.jsx
+++ src/Components/sections/OffersHome.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Offers } from "../../data";
 
 const OffersHome = () => {
+  const [hover, setHover] = useState(false);
+  let n = hover ? Offers[1].elemet.length : 5;
   return (
     <div className=" flex flex-col items-center justify-center  gap-10 ">
       <h1 className="highlighted-title  text-secondary tablet:text-[48px] text-[39px] ">
@@ -12,19 +14,19 @@ const OffersHome = () => {
           const { titel, elemet, bonus } = offer;
           return (
             <div
-              className={` hover:scale-50 transition-all shadow-lg p-6  ${
-                index == 0 ? `bg-secondary` : ``
+              className={`flex flex-col gap-6  p-6 rounded-md border-[1px] border-secondary  ${
+                index == 0 ? `bg-secondary text-primary ` : ``
               } `}
             >
-              <div className="flex justify-between ">
+              <div className="flex justify-between   ">
                 <h3>{titel}</h3>
                 {index == 1 && (
-                  <div className="bg-highlight py-[6px] px-[8px] rounded-lg text-primary  ">
+                  <div className="bg-highlight py-[6px] px-[8px] rounded-lg text-primary  text-[13px]  ">
                     Guaranteed
                   </div>
                 )}
               </div>
-              <div className=" relative ">
+              <div className=" relative    ">
                 <h1 className="text-[54px] font-bold  ">Free</h1>
                 {index == 1 && (
                   <p className="text-highlight absolute text-[15px] font-medium -bottom-1 left-24   ">
@@ -33,19 +35,23 @@ const OffersHome = () => {
                 )}
               </div>
 
-              <div>
-                {elemet.map((i) => {
+              <div
+                className="flex flex-col gap-4  "
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+              >
+                {elemet.slice(0, n).map((i) => {
                   return (
-                    <div>
+                    <div className="">
                       <img src="" alt="" />
                       <p>{i}</p>
                     </div>
                   );
                 })}
               </div>
-              <div>
-                <h2> Bonus </h2>
-                <div>
+              <div className="min-h-[230px] flex flex-col gap-4  ">
+                <h2 className={`text-[25px] font-bold `}> Bonus </h2>
+                <div className={`flex flex-col itmes-center gap-2   `}>
                   {bonus.map((b) => {
                     return (
                       <div>
@@ -55,6 +61,15 @@ const OffersHome = () => {
                     );
                   })}
                 </div>
+              </div>
+              <div className=" flex justify-between  justify-self-end	  ">
+                <button className="p-4 bg-highlight  rounded-xl">
+                  Get started
+                </button>
+                <button className="p-4 bg-highlight  rounded-xl">
+                  {" "}
+                  More Dettials{" "}
+                </button>
               </div>
             </div>
           );

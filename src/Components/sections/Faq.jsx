60e802615c8468b2e26abe7e59419b22711f976b
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-04 15:10:13 +0100

    ui complted Faq sestion Ui

--- src/Components/sections/Faq.jsx
+++ src/Components/sections/Faq.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaqData } from "../../data";
 const Faq = () => {
+  const [toggle, setToggle] = useState(null);
+  const toggleIndex = (index) => {
+    setToggle(toggle == index ? null : index);
+  };
+
   return (
     <>
       <div className=" p-10 w-full flex flex-col items-center justify-center  gap-14  ">
@@ -8,11 +13,26 @@ const Faq = () => {
           Offers
         </h1>
         <div className=" w-full flex flex-col  gap-8    ">
-          {FaqData.map((item) => {
+          {FaqData.map((item, index) => {
             const { question, answer } = item;
             return (
-              <div className="p-4 shadow-md  ">
-                <p className="text-[19px] text-secondary ">{question}</p>
+              <div className="p-6  shadow-md  " key={index}>
+                <div className="flex  items-center ">
+                  <button
+                    className="text-[28px] text-highlight w-[10%] "
+                    onClick={() => toggleIndex(index)}
+                  >
+                    {toggle == index ? "-" : "+"}
+                  </button>
+                  <h2>{question} </h2>
+                </div>
+                <div
+                  className={`${toggle === index ? `faq-open` : `faq-answer`}`}
+                >
+                  {answer.map((i) => {
+                    return <p>{i}</p>;
+                  })}
+                </div>
               </div>
             );
           })}

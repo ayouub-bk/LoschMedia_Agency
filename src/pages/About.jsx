d81888e15e416ac7f1b213ae987575f48fda38eb
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-04 18:37:30 +0100

    completed our-mission section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../Components/Ui/Button";
+import commas from "../assets/icons/icons8-comma-100.png";
 
 const About = () => {
   return (
@@ -16,8 +17,30 @@ const About = () => {
           <Button />
         </div>
       </header>
+      <div className="our-mission  ">
+        <div className="h-[250px] tablet:h-[420px] desktop:h-[370px] bg-secondary flex justify-center items-center ">
+          <h1 className=" text-primary text-[31px] tablet:text-[48px] ">
+            Our Mission
+          </h1>
+        </div>
+        <div className="flex flex-col items-center text-center justify-center p-4 gap-8  ">
+          <div className="flex">
+            <img src={commas} height="67" width="40" alt="" />
+            <img src={commas} height="67" width="40" alt="" />
+          </div>
 
-      <div className="tablet:p-8 desktop:p-14 p-4 "></div>
+          <p className="w-[80%] tablet:w-[70%]  tablet:text-[39px] text-[30px] text-secondary ">
+            At LoschMedia, we help B2B businesses grow by acquiring new clients.
+            Our founder, Chahid Kari, created a results-focused approach that
+            eliminates the risks of traditional agencies
+          </p>
+        </div>
+      </div>
+      <div className="bg-secondary h-[2px] w-full my-10">
+        <span className="opacity-0">.</span>
+      </div>
+
+      <div className="tablet:p-8 desktop:p-14 p-4  "></div>
     </>
   );
 };

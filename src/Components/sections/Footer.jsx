ba03009d3246039edf714edd9d5e0622829340d0
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-04 16:45:10 +0100

    footer-section Ui

--- src/Components/sections/Footer.jsx
+++ src/Components/sections/Footer.jsx
@@ -8,18 +8,30 @@ import youtub from "../../assets/icons/youtube.png";
 const Footer = () => {
   return (
     <>
-      <div className="w-full  flex justify-between ">
-        <div>
-          <img src={Logo} alt="" />
-          <h1 className="font-bold">Book a meeting now </h1>
-          <div className="flex justify-center ">
-            <Button />
-            <Button />
+      <div className="w-full  flex justify-between  bg-secondary py-[24px] px-[96px]  ">
+        <div className="flex flex-col  gap-6 items-center justify-between  ">
+          <img
+            className="rounded-lg object-fill "
+            src={Logo}
+            width="190px"
+            height="80"
+            alt=""
+          />
+          <h1 className="font-bold text-primary text-[24px]  ">
+            Book a meeting now{" "}
+          </h1>
+          <div className="flex justify-center gap-8 ">
+            <button className="p-2 bg-primary  text-secondary  rounded-lg  ">
+              Book a Call{" "}
+            </button>
+            <button className="p-2 bg-primary  text-secondary  rounded-lg  ">
+              More Dettails
+            </button>
           </div>
         </div>
         <div>
-          <h2>Links</h2>
-          <ul>
+          <h2 className="text-primary text-[25px]">Links</h2>
+          <ul className="text-primary text-[23px] flex flex-col gap-2 ">
             <li>
               <Link>Services</Link>
             </li>
@@ -35,8 +47,8 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <h2>Social Links</h2>
-          <ul>
+          <h2 className=" text-primary text-[25px] mb-2 ">Social Links</h2>
+          <ul className="social-links flex  flex-col  gap-6 items-center ">
             <li>
               <Link>
                 <img src={insta} alt="insta" />

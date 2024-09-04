e93330815ea1741c58c3dc3bdb6dca2008c45302
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-04 20:53:42 +0100

    compeleted special-section ui + data

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../Components/Ui/Button";
 import commas from "../assets/icons/icons8-comma-100.png";
 import { SpecialData } from "../data";
+import Logo from "../assets/imgs/Logo.jpg";
 
 const About = () => {
   return (
@@ -42,16 +43,16 @@ const About = () => {
       </div>
 
       <div className="tablet:p-8 desktop:p-20 p-4 flex flex-col items-center   ">
-        <section className="w-full">
-          <h1 className="highlighted-title font-bold text-center mb-20 text-[48px] text-secondary    ">
+        <section className="w-full ">
+          <h1 className="highlighted-title font-bold text-center mb-[34vh] text-[48px] text-secondary    ">
             {" "}
             Why we are special ?{" "}
           </h1>
-          <div className="flex flex-col gap-6  ">
+          <div className="flex flex-col gap-6 relative p-2 ">
             {SpecialData.map((special, index) => {
               const { icon, title, others, middle } = special;
               return (
-                <div className="flex justify-between  p-2 pb-4 items-center border-b-2 border-secondary ">
+                <div className="flex justify-between  p-2 pb-4 items-center border-b-2 border-secondary h-[120px]">
                   <div className="flex gap-2 ">
                     <img src={icon} height="40px" width="40px" alt="icon" />
                     <h1 className="text-center  text-[16px] tablet:text-[25px] text-bold ">
@@ -67,6 +68,25 @@ const About = () => {
                 </div>
               );
             })}
+            <div className="absolute flex flex-col items-center p-2    bottom-0  -top-30 right-[35%] bg-primary w-[30%] gap-6 shadow-lg rounded-md  ">
+              <img
+                className="mb-14"
+                src={Logo}
+                height="71px"
+                width="230px"
+                alt="logo"
+              />
+              {SpecialData.map((mid, index) => {
+                const { middle } = mid;
+                return (
+                  <div className="h-[120px] flex justify-center items-center  p-2  ">
+                    <h6 className="text-center text-[16px] font-bold tablet:text-[20px] text-secondary ">
+                      {middle}
+                    </h6>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </section>
       </div>

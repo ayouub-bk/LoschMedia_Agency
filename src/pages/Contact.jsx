aafffd158a86f179361535193a647e8a359c27e0
JuniorDev1k <ayoubdev1k@gmail.com>
2024-09-05 17:46:48 +0100

    completed Context-screen UI

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Button from "../Components/Ui/Button";
-
+import contactImg from "../assets/icons/Calendly-Demand-Gen-2.png";
 const Contact = () => {
   return (
-    <div>
+    <div className="">
       <header className=" bg-custom-radial py-20 px-10  flex justify-center items-center p-4 relative">
         <div className="flex flex-col gap-[30px] items-center desktop:w-[1008px] text-center text-primary ">
           <h1 className=" text-[31px] tablet:text-[61px] font-bold   ">
@@ -15,6 +15,9 @@ const Contact = () => {
           <Button />
         </div>
       </header>
+      <div className=" p-10  desktop:px-20   h-['730px'] w-['full']">
+        <img className="w-full h-full " src={contactImg} alt="img" />
+      </div>
     </div>
   );
 };
